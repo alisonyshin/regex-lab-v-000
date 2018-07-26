3593fdaed504514c6a8f0c09da6bd907ba327aef
Alison Shin <alisonyshin@gmail.com>
2018-07-26 01:32:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,6 +21,8 @@ end
 def valid_phone_number?(phone)
   all_digits = phone.scan(/\d/)
   all_digits.length == 10? true:false
+  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+
 end
 #valid_numbers = "(718)891-1313"
 #puts valid_phone_number?(valid_numbers)
